45a82d6f0ff7fb217017cbd90ece7c09c9cd8a67
Divyansh Saini <122964007+projectsby-saini@users.noreply.github.com>
2024-02-17 18:01:27 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,4 @@
+const add = 10 + 2;
+console.log("The added value is " + add); // Output: The added value is 12
+const subs = 10 - 2;
+console.log("The subtracted value is " + subs); // Output: The subtracted value is 8
